dec6fe8288eb2a1b5ef33d7a269d2e108baf5d9c
ZhariEldos <zhari00@proton.me>
2025-12-09 19:33:48 +0600

    operations

---
+++ operationJS.js
@@ -0,0 +1,22 @@
+let a = 10
+let b = 5
+let c = 2
+console.log(a + b + c)
+console.log(a + c - b)
+console.log(a * b / c)
+console.log(a % b)
+let x = 7
+console.log(x++ + ++x)
+console.log(x-- - --x)
+let number = 4 + 5
+number = number.toString()
+let string = "The result is: " + number
+console.log(string)
+console.log(a > b && a > 0 && b > 0)
+console.log(c < 10 || c == 2)
+console.log(a % c != 5)
+console.log(10 == "10") // Здесь просто сравнивают числа без учёта типа данных
+console.log(10 === "10") // А здесь в точности да наоборот, сравнивают два значение с учётом типа данных
+console.log((x % 2 == 0 || x % 3 == 0) && x % 6 != 0)
+let y = 10
+console.log(y > 10 || y < 5)
