74d8b866407a7e47d73f0b098895f143e66ffe56
ZhariEldos <zhari00@proton.me>
2026-02-13 19:42:49 +0600

    Async JS

---
+++ async.js
@@ -0,0 +1,97 @@
+var now = new Date();
+
+setTimeout(() => {
+    console.log('Hello World!');
+}, 2000); 
+
+var i = 0;
+const intervalId = setInterval(() => {
+    now = new Date();
+    console.log(now);
+    i = i + 1;
+    if(i>=5) {
+        clearInterval(intervalId);
+    }
+}, 1000);
+const fetchData = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Data recieved!")
+    }, 3000);
+})
+fetchData.then(result => console.log(result))
+const fetchDataWithError = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject("Error!")
+    }, 2000);
+})
+fetchDataWithError.catch(error => console.log(error))
+async function fetchDataAsync() {
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Data recieved!!!");
+        }, 3000);
+    })
+    let result = await promise;
+    return result
+}
+fetchDataAsync().then(result => console.log(result))
+
+async function fetchDataAsyncError() {
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("Error!!!")
+        }, 2000);
+    })
+    try {
+        let result = await promise;
+        console.log(result)
+    } catch(error) {
+        console.log(error)
+    }
+}
+fetchDataAsyncError()
+
+const task1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Everything good 1")
+    }, 1000);
+})
+const task2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Everything good 2")
+    }, 2000);
+})
+async function task12Control() {
+    let promise1 = new Promise((resolve, reject) => {
+        resolve(task1.then())
+    })
+    let promise2 = new Promise((resolve, reject) => {
+        resolve(task2.then())
+    })
+    let result1 = await promise1;
+    console.log(result1)
+    let result2 = await promise2;
+    console.log(result2)
+}
+task12Control()
+
+const taskA = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Everything good 1")
+    }, 2000);
+})
+const taskB = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Everything good 2")
+    }, 3000);
+})
+Promise.all([taskA,taskB]).then(
+    (value) => {console.log(value)}
+)
+
+async function delayedMessage(message, interval) {
+    setTimeout(() => {
+        console.log(message)
+    }, interval);
+}
+delayedMessage("Delayed message", 1000)
